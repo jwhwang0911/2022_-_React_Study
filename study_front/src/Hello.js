c3cc88bcc4ec2f25d228d02d66dc97edf41a66e8
jwhwang0911 <star4007lg@gmail.com>
2022-02-02 17:21:36 +0900

    React #01-05 Component 값 전달; 비구조화 할당

--- study_front/src/Hello.js
+++ study_front/src/Hello.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Hello(props) { // props는 객체로 넘어옴
+function Hello({ name, color }) { // props는 객체로 넘어옴 / props 객체 사용 안하면 변수를 설정
     return (
-        <div style={{color : props.color}}>
-            안녕하세요 {props.name}
+        <div style={{ color }}>
+            안녕하세요 {name}
         </div>
     )
 }
