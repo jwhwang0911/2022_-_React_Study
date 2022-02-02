4901936b7ac85114f919ded37c5aba9ec3054279
jwhwang0911 <star4007lg@gmail.com>
2022-02-02 17:54:51 +0900

    React #01-07 Console.log

--- study_front/src/App.js
+++ study_front/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Hello from './Hello';
 import './App.css';
 import Wrapper from "./Wrapper";
+import Counter from "./Counter";
 
 function App() {
 
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial = {true}/>
-      <Hello color="blue"/>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Hello name="react" color="red" isSpecial = {true}/>
+        <Hello color="blue"/>
+      </Wrapper>
+      <Counter />
+    </>
   );
 }
 
