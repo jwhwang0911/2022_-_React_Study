a515cc2e0602d397c0ac5a3e3b553fae63a270fb
jwhwang0911 <star4007lg@gmail.com>
2022-02-02 17:33:41 +0900

    React #01-05 Wrapper; 쓰임 당하는 곳에서는 매개변수 : children

--- study_front/src/App.js
+++ study_front/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
 import './App.css';
+import Wrapper from "./Wrapper";
 
 function App() {
 
   return (
-    <>
+    <Wrapper>
       <Hello name="react" color="red"/>
       <Hello color="blue"/>
-    </>
+    </Wrapper>
   );
 }
 
