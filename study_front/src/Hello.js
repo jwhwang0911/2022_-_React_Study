11010608997feee84b1e53785f15cd68bf275bb0
jwhwang0911 <star4007lg@gmail.com>
2022-02-02 17:42:28 +0900

    React #01-06 Conditional Rendering

--- study_front/src/Hello.js
+++ study_front/src/Hello.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function Hello({ name, color }) { // props는 객체로 넘어옴 / props 객체 사용 안하면 변수를 설정
+function Hello({ name, color,isSpecial }) { // props는 객체로 넘어옴 / props 객체 사용 안하면 변수를 설정
     return (
         <div style={{ color }}>
+            { isSpecial && <b>*</b> }
             안녕하세요 {name}
         </div>
-    )
+    );
 }
 
 Hello.defaultProps = {
