3e11ac574335f58de411e8854837146de6fbe8f8
jwhwang0911 <star4007lg@gmail.com>
2022-02-07 17:28:00 +0900

    React #01-11 Rendering List

---
+++ study_front/src/UserList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// 한파일에 여러개의 컴포넌트 선언해도 ㄱㅊ
+function User({ user }) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    );
+}
+
+function UserList() {
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+        },
+        {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+        }
+    ];
+
+    return (
+        <div>
+            <User user={users[0]} />
+            <User user={users[1]} />
+            <User user={users[2]} />
+        </div>
+    )
+}
