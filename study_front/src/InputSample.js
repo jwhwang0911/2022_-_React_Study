c5a0e48d666d5bc8f7e16d8d7b10dbfb34e46d79
jwhwang0911 <star4007lg@gmail.com>
2022-02-07 17:18:27 +0900

    React #01-10 useRef Dom select

--- study_front/src/InputSample.js
+++ study_front/src/InputSample.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name : '',
         nickname : ''
     });
+    const nameInput = useRef();
+
     const {name, nickname} = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
@@ -12,13 +14,14 @@ function InputSample() {
         setInputs({
             ...inputs, //기존의 input 객체를 복사한뒤
             [name]: value // name 키를 가진 값을 value로 설정
-        })
+        });
     };
     const onReset = () => {
         setInputs({
             name: '',
             nickname: '',
-        })
+        });
+        nameInput.current.focus();
     };
 
     // placeholder는 인풋 텍스트에 배경깔아주는거
