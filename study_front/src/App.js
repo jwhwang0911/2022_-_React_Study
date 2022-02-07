9ba000f9cdc35987fccc928a44742abfb6bf0470
jwhwang0911 <star4007lg@gmail.com>
2022-02-07 17:32:53 +0900

    React #01-11 Rendering List by Map

--- study_front/src/App.js
+++ study_front/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       </Wrapper>
       <Counter />
       <InputSample />
+      <UserList />
     </>
   );
 }
--- study_front/src/UserList.js
+++ study_front/src/UserList.js
@@ -17,22 +17,24 @@ function UserList() {
             email: 'public.velopert@gmail.com'
         },
         {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com'
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
         },
         {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
         }
     ];
 
     return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {users.map(user => (
+                <User user={user} key = {user.id} />
+            ))}
         </div>
     )
 }
+
+export default UserList;
